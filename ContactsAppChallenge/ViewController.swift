0ba447b9fd009f0ee169772a15c857b67e51373a
Eric Widjaja <ericwidjaja@pursuit.org>
2020-11-05 15:23:59 -0500

    create new project for AppsChallenge

---
+++ ContactsAppChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ContactsAppChallenge
+//
+//  Created by Eric Widjaja on 11/5/20.
+//  Copyright © 2020 ericSwidjaja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
