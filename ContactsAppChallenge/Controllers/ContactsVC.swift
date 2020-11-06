ef3fd6c0eccdbc9af0905b622399ed4b3208fcda
Eric Widjaja <ericwidjaja@pursuit.org>
2020-11-06 17:51:52 -0500

    grouped file into folders

--- ContactsAppChallenge/Controllers/ContactsVC.swift
+++ ContactsAppChallenge/Controllers/ContactsVC.swift
@@ -1,12 +1,13 @@
 //  ViewController.swift
 //  ContactsAppChallenge
+
 //  Created by Eric Widjaja on 11/5/20.
 //  Copyright © 2020 ericSwidjaja. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ContactsVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
